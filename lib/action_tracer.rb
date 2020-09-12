16e4b7ab8f8a0ab5a440e5b5d729a30829721967
makicamel <unright@gmail.com>
2020-09-13 07:36:46 +0900

    Add magic comment

--- lib/action_tracer.rb
+++ lib/action_tracer.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 require "action_tracer/version"
 require "action_tracer/file_path_checker"
 require "action_tracer/logger"
--- lib/action_tracer/file_path_checker.rb
+++ lib/action_tracer/file_path_checker.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module ActionTracer
   class FilePathChecker
     def initialize(pwd_path)
