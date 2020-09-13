3c1fd1bd0aec8cda826cb14229a741e8e54c177c
makicamel <unright@gmail.com>
2020-09-13 19:25:03 +0900

    Reduce calling TracePoint

--- lib/action_tracer/action_tracer.rb
+++ lib/action_tracer/action_tracer.rb
@@ -3,23 +3,27 @@
 module ActionTracer
   class Error < StandardError; end
 
-  callback_caller = nil
-
-  TracePoint.trace(:call) do |tp|
-    if callback_caller
+  def self.caller
+    @caller ||= TracePoint.new(:call) do |tp|
       if FilePathChecker.app?(tp.path)
         log = "#{tp.method_id}@#{tp.path}:#{tp.lineno} #{tp.defined_class}"
         ActionTracer.logger.info log
         Rails.logger.info "[ACTION_TRACER] #{log}"
-        callback_caller = nil
+        caller.disable
+        returner.enable
       end
     end
   end
 
-  TracePoint.trace(:return) do |tp|
-    # NOTE: ActiveSupport::Callbacks::CallTemplate is a private class
-    if tp.method_id == :expand && tp.defined_class == ActiveSupport::Callbacks::CallTemplate
-      callback_caller = tp.return_value.last
+  def self.returner
+    @returner ||= TracePoint.new(:return) do |tp|
+      # NOTE: ActiveSupport::Callbacks::CallTemplate is a private class
+      if tp.method_id == :expand && tp.defined_class == ActiveSupport::Callbacks::CallTemplate
+        returner.disable
+        caller.enable
+      end
     end
   end
+
+  returner.enable
 end
