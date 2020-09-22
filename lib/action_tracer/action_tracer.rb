3b07385cf3b475cd90dcbdbef8ee5965c93ab2bd
makicamel <unright@gmail.com>
2020-09-22 15:40:37 +0900

    Quit to use TracePoint and just override

--- lib/action_tracer/action_tracer.rb
+++ lib/action_tracer/action_tracer.rb
@@ -2,17 +2,4 @@
 
 module ActionTracer
   class Error < StandardError; end
-
-  def self.returner
-    @returner ||= TracePoint.new(:return) do |tp|
-      if tp.defined_class == AbstractController::Base && tp.method_id == :process
-        target = tp.binding.receiver
-        filters = target.__callbacks[:process_action].send(:chain).map(&:filter)
-        filters.each do |filter|
-          # TODO: when filter is Proc, log line_no
-          ActionTracer.logger.info target.method(filter).source_location.unshift(filter) if filter.is_a? Symbol
-        end
-      end
-    end
-  end
 end
--- lib/action_tracer/monkey_patches/abstract_controller/base.rb
+++ lib/action_tracer/monkey_patches/abstract_controller/base.rb
@@ -4,10 +4,21 @@ module ActionTracer
   module MonkeyPatches
     module AbstractController
       module Base
-        def process(action, *args)
-          ActionTracer.returner.enable
-          super
-          ActionTracer.returner.disable
+        private
+
+        # Monkey patch to log filters
+        def process_action(action, *args)
+          super.tap { logging }
+        end
+
+        def logging
+          filters = __callbacks[:process_action].send(:chain).group_by(&:kind).map { |kind, callbacks| [kind, callbacks.map(&:filter)] }.to_h
+          # TODO: when filter is Proc, log line_no
+          log = -> (method) { ActionTracer.logger.info method(method).source_location.unshift(method) if method.is_a? Symbol }
+          [:before, :around].each { |key| filters[key].each(&log) }
+          filters[:around].reverse_each(&log)
+          filters[:after].each(&log)
+          ActionTracer.logger.info ""
         end
       end
     end
