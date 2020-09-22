dca08585a4c489276d8d4a2c0dda4a05f8aef5b3
makicamel <unright@gmail.com>
2020-09-22 17:33:27 +0900

    Move logging method out of monkey patch

--- lib/action_tracer/action_tracer.rb
+++ lib/action_tracer/action_tracer.rb
@@ -2,4 +2,15 @@
 
 module ActionTracer
   class Error < StandardError; end
+
+  def self.logging(controller)
+    filters = controller.__callbacks[:process_action].send(:chain).group_by(&:kind).map { |kind, callbacks| [kind, callbacks.map(&:filter)] }.to_h
+    # TODO: when filter is Proc, log line_no
+    log = -> (method) { ActionTracer.logger.info controller.method(method).source_location.unshift(method) if method.is_a? Symbol }
+    [:before, :around].each { |key| filters[key].each(&log) }
+    log.call(controller.action_name.to_sym)
+    filters[:around].reverse_each(&log)
+    filters[:after].each(&log)
+    ActionTracer.logger.info ""
+  end
 end
--- lib/action_tracer/monkey_patches/abstract_controller/base.rb
+++ lib/action_tracer/monkey_patches/abstract_controller/base.rb
@@ -8,18 +8,7 @@ module ActionTracer
 
         # Monkey patch to log filters
         def process_action(action, *args)
-          super.tap { logging }
-        end
-
-        def logging
-          filters = __callbacks[:process_action].send(:chain).group_by(&:kind).map { |kind, callbacks| [kind, callbacks.map(&:filter)] }.to_h
-          # TODO: when filter is Proc, log line_no
-          log = -> (method) { ActionTracer.logger.info method(method).source_location.unshift(method) if method.is_a? Symbol }
-          [:before, :around].each { |key| filters[key].each(&log) }
-          log.call(action_name.to_sym)
-          filters[:around].reverse_each(&log)
-          filters[:after].each(&log)
-          ActionTracer.logger.info ""
+          super.tap { ActionTracer.logging(self) }
         end
       end
     end
