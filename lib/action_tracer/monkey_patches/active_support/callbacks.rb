37c56dbb039fbdf61bef7544158936c3627200c2
makicamel <unright@gmail.com>
2022-05-01 20:11:43 +0900

    Deal with Rails7.x
    Follow https://github.com/rails/rails/pull/43713

--- lib/action_tracer/monkey_patches/active_support/callbacks.rb
+++ lib/action_tracer/monkey_patches/active_support/callbacks.rb
@@ -5,20 +5,64 @@ module ActionTracer
     module ActiveSupport
       module Callbacks
         module CallTemplate
-          def expand(*)
-            target, block, method, *arguments = super
-            if target.is_a? ActionController::Base
-              case method
-              when :instance_exec # filter is a proc
-                ActionTracer.applied_filters << block
-              when String         # filter is an object
-                ActionTracer.applied_filters << block
-              when Symbol         # filter is a method
-                ActionTracer.applied_filters << method
+          if Rails::VERSION::MAJOR > 6
+            module MethodCall
+              def expand(*)
+                super.tap { ActionTracer.applied_filters << @method_name }
+              end
+
+              def make_lambda
+                super >> proc { ActionTracer.applied_filters << @method_name }
+              end
+
+              def inverted_lambda
+                super >> proc { ActionTracer.applied_filters << @method_name }
               end
             end
 
-            [target, block, method, *arguments]
+            module ProcCall
+              def expand(*)
+                super.tap { ActionTracer.applied_filters << @override_target }
+              end
+
+              def make_lambda
+                super >> proc { ActionTracer.applied_filters << @override_target }
+              end
+
+              def inverted_lambda
+                super >> proc { ActionTracer.applied_filters << @override_target }
+              end
+            end
+
+            module InstanceExec
+              def expand(*)
+                super.tap { ActionTracer.applied_filters << @override_block }
+              end
+
+              def make_lambda
+                super >> proc { ActionTracer.applied_filters << @override_block }
+              end
+
+              def inverted_lambda
+                super >> proc { ActionTracer.applied_filters << @override_block }
+              end
+            end
+          else
+            def expand(*)
+              target, block, method, *arguments = super
+              if target.is_a? ActionController::Base
+                case method
+                when :instance_exec # filter is a proc
+                  ActionTracer.applied_filters << block
+                when String         # filter is an object
+                  ActionTracer.applied_filters << block
+                when Symbol         # filter is a method
+                  ActionTracer.applied_filters << method
+                end
+              end
+
+              [target, block, method, *arguments]
+            end
           end
         end
       end
--- lib/action_tracer/railtie.rb
+++ lib/action_tracer/railtie.rb
@@ -23,7 +23,29 @@ module ActionTracer
     initializer "action_tracer" do
       ActiveSupport.on_load(:action_controller) do
         require "action_tracer/monkey_patches/active_support/callbacks"
-        ::ActiveSupport::Callbacks::CallTemplate.prepend ActionTracer::MonkeyPatches::ActiveSupport::Callbacks::CallTemplate
+        if Rails::VERSION::MAJOR > 6
+          [
+            ::ActiveSupport::Callbacks::CallTemplate::MethodCall,
+            ::ActiveSupport::Callbacks::CallTemplate::ObjectCall,
+          ].each do |klass|
+            klass.prepend ActionTracer::MonkeyPatches::ActiveSupport::Callbacks::CallTemplate::MethodCall
+          end
+          [
+            ::ActiveSupport::Callbacks::CallTemplate::InstanceExec0,
+            ::ActiveSupport::Callbacks::CallTemplate::InstanceExec1,
+            ::ActiveSupport::Callbacks::CallTemplate::InstanceExec2,
+          ].each do |klass|
+            klass.prepend ActionTracer::MonkeyPatches::ActiveSupport::Callbacks::CallTemplate::InstanceExec
+          end
+          [
+            ::ActiveSupport::Callbacks::CallTemplate::ProcCall,
+          ].each do |klass|
+            klass.prepend ActionTracer::MonkeyPatches::ActiveSupport::Callbacks::CallTemplate::ProcCall
+          end
+        else
+          ::ActiveSupport::Callbacks::CallTemplate.prepend ActionTracer::MonkeyPatches::ActiveSupport::Callbacks::CallTemplate
+        end
+
         require "action_tracer/monkey_patches/abstract_controller/callbacks"
         ::ActionController::Base.prepend ActionTracer::MonkeyPatches::AbstractController::Callbacks
 
